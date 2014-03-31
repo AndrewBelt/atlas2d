da981591053adfcc1ce679655ce1f76617c18337
Luke Bechtel <lukebechtel4@gmail.com>
2014-03-31 16:29:35 -0400

    Began Construction Server Model in Python.

---
+++ server/python/model.py
@@ -0,0 +1,13 @@
+class Connection(object):
+  def __init__(self):
+
+
+class Client(object):
+  def __init__(self):
+    #id
+    #connection
+    #
+
+class Entity(object):
+  def __init__(self):
+
